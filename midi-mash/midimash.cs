7bc201f892ba7bcf3170a16b6cd7f049f46c37d9
charlierobson <charlie.robson@b2m-solutions.com>
2017-06-26 09:32:36 +0100

    use metadata in file to calculate rates

--- midi-mash/midimash.cs
+++ midi-mash/midimash.cs
@@ -69,7 +69,7 @@ public static class mp
             var timeSignatureDenominator = Math.Pow(2, int.Parse(timeSigParts[4]));
 
             var bpm = (oneMinuteInMicroseconds / microsecondsPerQuarterNote) * (timeSignatureDenominator / timeSignatureNumerator);
-            Console.WriteLine($"BPM: {bpm}");
+            VerboseWriteLine($"BPM: {bpm}");
 
             var secondsPerQuarterNote = microsecondsPerQuarterNote / 1000000.0f;
             var secondsPerTick = secondsPerQuarterNote / ticksPerQuarterNote;
@@ -83,6 +83,8 @@ public static class mp
                 var components = line.Split(',').Select(p => p.Trim()).ToArray();
                 if (msgMap.Keys.Contains(components[2]))
                 {
+                    var msg = msgMap[components[2]](components);
+
                     var deltaTimeInFiftieths = (int)(int.Parse(components[1]) * fiftiethsPerTick);
                     if (!midi.ContainsKey(deltaTimeInFiftieths))
                     {
@@ -90,7 +92,8 @@ public static class mp
                     }
 
                     var midiData = midi[deltaTimeInFiftieths];
-                    midiData.AddRange(msgMap[components[2]](components));
+
+                    midiData.AddRange(msg);
 
                     biggestBlock = Math.Max(biggestBlock, midiData.Count);
                     lastTimeStamp = deltaTimeInFiftieths;
@@ -101,30 +104,38 @@ public static class mp
                 }
             }
 
-            const int HeaderByteCount = 3;
+            const int blockSize = 256;
+            const int HeaderByteCount = 4;
 
-            var minBlockSize = (int)(Math.Pow(2, Math.Ceiling(Math.Log(biggestBlock + HeaderByteCount)/Math.Log(2))));
+            //var minBlockSize = (int)(Math.Pow(2, Math.Ceiling(Math.Log(biggestBlock + HeaderByteCount)/Math.Log(2))));
+            //Console.WriteLine($"Block size is {minBlockSize} bytes.");
 
-            Console.WriteLine($"Block size is {minBlockSize} bytes.");
+            if (lastTimeStamp >= Math.Pow(2,24))
+                Console.WriteLine("Error: time stamp cannot be represented by 24 bit value.");
 
-            if (lastTimeStamp > UInt16.MaxValue)
-                Console.WriteLine("Error: time stamp cannot be represented by 16 bit value.");
-
-            var rawFile = new List<byte>();
-            foreach(var key in midi.Keys)
+            if (biggestBlock > blockSize - HeaderByteCount)
             {
-                // header = (word) required frame number, midi packet size in bytes
-                rawFile.Add((byte)(key & 255));
-                rawFile.Add((byte)(key / 256));
-                rawFile.Add((byte)(midi[key].Count));
-
-                rawFile.AddRange(midi[key]);
-
-                var blkRemain = minBlockSize - HeaderByteCount - midi[key].Count;
-                rawFile.AddRange(new byte[blkRemain]);
+                Console.WriteLine("Error: block size exceeds maximum.");
             }
+            else
+            {
+                var rawFile = new List<byte>();
+                foreach(var key in midi.Keys)
+                {
+                    // header = frame number @ 24 bits, midi packet size in bytes @ 8 bits
+                    rawFile.Add((byte)((key & 0x0000ff)));
+                    rawFile.Add((byte)((key & 0x00ff00) >> 8));
+                    rawFile.Add((byte)((key & 0xff0000) >> 16));
+                    rawFile.Add((byte)(midi[key].Count));
 
-            File.WriteAllBytes(Path.ChangeExtension(args[0], ".zxm"), rawFile.ToArray());
+                    rawFile.AddRange(midi[key]);
+
+                    var blkRemain = blockSize - HeaderByteCount - midi[key].Count;
+                    rawFile.AddRange(new byte[blkRemain]);
+                }
+
+                File.WriteAllBytes(Path.ChangeExtension(args[0], ".zxm"), rawFile.ToArray());
+            }
         }
         catch(Exception ex)
         {
