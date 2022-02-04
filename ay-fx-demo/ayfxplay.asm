1625a99e533ef97050629181769f0e6afba1ed7b
charlierobson <charlie.robson@b2m-solutions.com>
2022-02-04 17:10:59 +0000

    bugfixed ayfx player

--- ay-fx-demo/ayfxplay.asm
+++ ay-fx-demo/ayfxplay.asm
@@ -1,35 +1,43 @@
-;--------------------------------------------------------------;
-;                                                              ;
-;The simplest effects player.                                  ;
-;                                                              ;
-;Plays effects on one AY, without music on the background.     ;
-;                                                              ;
-;Priority of channel selection:                                ;
-; If there are free channels, one of them is selected.         ;
-; If there are no free channels, the longest sounding          ;
-; is selected.                                                 ;
-;                                                              ;
-;The playing procedure uses the registers AF, BC, DE, HL, IY.  ;
-;                                                              ;
-; Initialization:                                              ;
-;   ld hl, effects bank address                                ;
-;   call AFXINIT                                               ;
-;                                                              ;
-; Start the effect:                                            ;
-;   ld a, effect number (1..255)                               ;
-;   call AFXPLAY                                               ;
-;                                                              ;
-; In the interrupt handler:                                    ;
-;   call AFXFRAME                                              ;
-;                                                              ;
-;--------------------------------------------------------------;
+	.module AFX
+
+;---------------------------------------------------------------;
+;                                                               ;
+; The simplest effects player.                                  ;
+;                                                               ;
+; Plays effects on one AY, without music on the background.     ;
+;                                                               ;
+; Priority of channel selection:                                ;
+;  If there are free channels, one of them is selected.         ;
+;  If there are no free channels, the longest sounding          ;
+;  is selected.                                                 ;
+;                                                               ;
+; The playing procedure uses the registers AF, BC, DE, HL, IY.  ;
+;                                                               ;
+;  Initialization:                                              ;
+;    ld hl, effects bank address                                ;
+;    call AFXINIT                                               ;
+;                                                               ;
+;  Start the effect:                                            ;
+;    ld a, effect number (1..255)                               ;
+;    call AFXPLAY                                               ;
+;                                                               ;
+;  In the interrupt handler:                                    ;
+;    call AFXFRAME                                              ;
+;                                                               ;
+;---------------------------------------------------------------;
+
+
+;--------------------------------;
+; Bugfixed and polished Feb 2022 ;
+;--------------------------------;
+
 
 ; channel descriptors, 4 bytes per channel:
 ; +0 (2) current address (the channel is free if the high byte =$00)
 ; +2 (2) sound time
 ; ...
 
-afxChDesc	.fill 12 ; 3 channels * 4 bytes
+_chdesc	.fill 12 		; 3 channels * 4 bytes
 
 
 ;--------------------------------------------------------------;
@@ -38,36 +46,98 @@ afxChDesc	.fill 12 ; 3 channels * 4 bytes
 ; Input: HL = effects bank address                             ;
 ;--------------------------------------------------------------;
 
-AFXINIT:
+INIT:
 	inc hl
-	ld (afxBnkAdr+1),hl	;save the address of the table of offsets
+	ld (afxBnkAdr),hl	; save the address of the table of offsets
 
-	ld hl,afxChDesc		;mark all channels as empty
+	ld hl,_chdesc		; mark all channels as empty
 	ld de,$00ff
 	ld bc,$0300
-afxInit0
-	ld (hl),d
-	inc hl
-	ld (hl),d
-	inc hl
-	ld (hl),e
-	inc hl
-	ld (hl),e
-	inc hl
-	djnz afxInit0
 
-	ld hl,$cf0f			;initialize AY
+-:	ld (hl),d
+	inc hl
+	ld (hl),d
+	inc hl
+	ld (hl),e
+	inc hl
+	ld (hl),e
+	inc hl
+	djnz {-}
+
+	ld hl,$cf0f			; initialize AY
 	ld e,15
-afxInit1
-	dec e
+
+-:	dec e
 	ld c,h
 	out (c),e
 	ld c,l
 	out (c),d
-	jr nz,afxInit1
+	jr nz,{-}
 
-	ld (afxNseMix+1),de	;reset the player variables
+	ld (afxNseMix),de	; reset the player variables
 	ret
+
+
+
+
+
+
+;--------------------------------------------------------------;
+; Launch the effect on a free channel. Without                 ;
+; free channels is selected the longest sounding.              ;
+; Input: A = Effect number 1..255                              ;
+;--------------------------------------------------------------;
+
+PLAY:
+	dec a
+	ld de,0				; in DE the longest time in search
+	ld h,e
+	ld l,a
+	add hl,hl
+afxBnkAdr=$+1
+	ld bc,0				; self modifies: address of the effect offsets table
+	add hl,bc
+	ld c,(hl)
+	inc hl
+	ld b,(hl)
+	add hl,bc			; the effect address is obtained in hl
+
+	push hl				; save the effect address on the stack
+	
+	ld hl,_chdesc		; empty channel search
+	ld b,3
+
+-:	inc hl
+	inc hl
+	ld a,(hl)			; compare the channel time with the largest
+	inc hl
+	cp e
+	jr c,{+}
+
+	ld c,a
+	ld a,(hl)
+	cp d
+	jr c,{+}
+
+	ld e,c				; remember the longest time
+	ld d,a
+	ld (afxFreeChan),hl
++:	inc hl
+	djnz {-}
+
+	pop de				; take the effect address from the stack
+afxFreeChan=$+1
+	ld hl,0				; self modifies: address of free channel descriptor
+	ld (hl),b
+	dec hl
+	ld (hl),b
+	dec hl
+	ld (hl),d
+	dec hl
+	ld (hl),e
+	ret
+
+
 
 
 
@@ -76,40 +146,40 @@ afxInit1
 ; No parameters      .                                         ;
 ;--------------------------------------------------------------;
 
-AFXFRAME:
+FRAME:
 	ld bc,$0300
-	ld iy,afxChDesc
+	ld iy,_chdesc
 
-afxFrame0
+_frameLoop:
 	push bc
 
-	ld a,11				;needs to be 11 - see volume register selection
-	ld h,(iy+1)			;the comparison of the high-order byte of the address to < 11
+	ld a,11				; initial ay register value used below, also shonky comparison
+	ld h,(iy+1)			; the comparison of the high-order byte of the address to < $40
 	cp h
-	jr nc,afxFrame7		;the channel does not play, we skip
+	jr nc,_nextChan		; the channel does not play, we skip
 
 	ld l,(iy+0)
-	ld e,(hl)			;we take the value of the information byte
+	ld e,(hl)			; we take the value of the information byte
 	inc hl
 
-	sub b				;select the volume register:
-	ld d,b				;(11-3=8, 11-2=9, 11-1=10)
+	sub b				; select the volume register:
+	ld d,b				; (11-3=8, 11-2=9, 11-1=10)
 
-	ld c,$cf			;output the volume value
+	ld c,$cf			; output the volume value
 	out (c),a
 	ld a,e
 	and $0f
 	ld c,$0f
 	out (c),a
 	
-	bit 5,e				;will change the tone?
-	jr z,afxFrame1		;tone does not change
+	bit 5,e				; will change the tone?
+	jr z,{+}			; tone does not change
 	
-	ld a,3				;select the tone registers:
-	sub d				;3-3=0, 3-2=1, 3-1=2
-	add a,a				;0*2=0, 1*2=2, 2*2=4
+	ld a,3				; select the tone registers:
+	sub d				; 3-3=0, 3-2=1, 3-1=2
+	add a,a				; 0*2=0, 1*2=2, 2*2=4
 	
-	ld c,$cf			;output the tone values
+	ld c,$cf			; output the tone values
 	out (c),a
 	ld d,(hl)
 	inc hl
@@ -123,64 +193,59 @@ afxFrame0
 	ld c,$0f
 	out (c),d
 	
-afxFrame1
-	bit 6,e				;will change the noise?
-	jr z,afxFrame3		;noise does not change
++:	bit 6,e				; will change the noise?
+	jr z,_processUpdate	; noise does not change
 	
-	ld a,(hl)			;read the meaning of noise
+	ld a,(hl)			; read the meaning of noise
 	sub $20
-	jr c,afxFrame2		;less than $ 20, play on
+	jr c,{+}			; less than $ 20, play on
 
-	ld h,a				;otherwise the end of the effect
+	ld h,a				; otherwise the end of the effect
 	ld l,h
 	ld b,$ff
 	ld c,b
-	jr afxFrame6
+	jr _updateFxPtrs
 
-afxFrame2
-	inc hl
-	ld (afxNseMix+1),a	;keep the noise value
-	
-afxFrame3
-	pop bc				;restore the value of the cycle in B
++:	inc hl
+	ld (afxNseMix),a	; keep the noise value
+
+_processUpdate:
+	pop bc				; restore the value of the cycle in B
 	push bc
-	inc b				;number of shifts for flags TN
-	
-	ld a,%01101111		;mask for flags TN
-afxFrame4
-	rrc e				;shift flags and mask
+	inc b				; number of shifts for flags TN
+	ld a,%01101111		; mask for flags TN
+-:	rrc e				; shift flags and mask
 	rrca
-	djnz afxFrame4
+	djnz {-}
+
 	ld d,a
-	
-	ld bc,afxNseMix+2	;store the values ​​of the flags
+
+	ld bc,afxNseMix+1	; store the values ​​of the flags
 	ld a,(bc)
 	xor e
 	and d
-	xor e				;E is masked by D
+	xor e				; E is masked by D
 	ld (bc),a
-	
-afxFrame5
-	ld c,(iy+2)			;increase the time counter
+
+	ld c,(iy+2)			; increase the time counter
 	ld b,(iy+3)
 	inc bc
 	
-afxFrame6
+_updateFxPtrs:
 	ld (iy+2),c
 	ld (iy+3),b
-	
-	ld (iy+0),l			;save the changed address
+	ld (iy+0),l			; save the changed address
 	ld (iy+1),h
 	
-afxFrame7
-	ld bc,4				;go to the next channel
+_nextChan:
+	ld bc,4				; go to the next channel
 	add iy,bc
 	pop bc
-	djnz afxFrame0
+	djnz _frameLoop
 
-	ld hl,$cf0f			;output the value of noise and mixer
-afxNseMix
-	ld de,0				;+1 (E) = noise, +2 (D) = mixer
+	ld hl,$cf0f			; output the value of noise and mixer
+afxNseMix=$+1
+	ld de,0				; self modifies: +0 (E) = noise, +1 (D) = mixer
 	ld a,6
 	ld c,h
 	out (c),a
@@ -193,61 +258,3 @@ afxNseMix
 	out (c),d
 	
 	ret
-
-
-
-;--------------------------------------------------------------;
-; Launch the effect on a free channel. Without                 ;
-; free channels is selected the longest sounding.              ;
-; Input: A = Effect number 1..255                              ;
-;--------------------------------------------------------------;
-
-AFXPLAY:
-	dec a
-	ld de,0				;in DE the longest time in search
-	ld h,e
-	ld l,a
-	add hl,hl
-afxBnkAdr
-	ld bc,0				;address of the effect offsets table, self modified
-	add hl,bc
-	ld c,(hl)
-	inc hl
-	ld b,(hl)
-	add hl,bc			;the effect address is obtained in hl
-
-	push hl				;save the effect address on the stack
-	
-	ld hl,afxChDesc		;empty channel search
-	ld b,3
-afxPlay0
-	inc hl
-	inc hl
-	ld a,(hl)			;compare the channel time with the largest
-	inc hl
-	cp e
-	jr c,afxPlay1
-	ld c,a
-	ld a,(hl)
-	cp d
-	jr c,afxPlay1
-	ld e,c				;remember the longest time
-	ld d,a
-	ld (afxFreeChan+1),hl
-afxPlay1
-	inc hl
-	djnz afxPlay0
-
-	pop de				;take the effect address from the stack
-afxFreeChan
-	ld hl,0				;self modifies
-	ld (hl),b			;b is 0
-	dec hl
-	ld (hl),b
-	dec hl
-	ld (hl),d
-	dec hl
-	ld (hl),e
-	ret
-
-
